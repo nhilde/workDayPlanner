9b170bc6cfa92a729255a2fba048efb4f6540849
Noah Hildebrand <nhilde21@gmail.com>
2022-05-26 23:43:30 -0400

    worked on getting local storage and appending it to page

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,14 @@
 var today = moment();
 let timeBlock = document.querySelectorAll(".time-block")
+const nine = document.getElementById("9")
+const ten = document.getElementById("10")
+const eleven = document.getElementById("11")
+const twelve = document.getElementById("12")
+const one = document.getElementById("1")
+const two = document.getElementById("2")
+const three = document.getElementById("3")
+const four = document.getElementById("4")
+const five = document.getElementById("5")
 
 
 $(document).ready(function () {
@@ -11,6 +20,18 @@ $(document).ready(function () {
         localStorage.setItem(time, plannerText)
 
     });
+
+    nine.innerHTML =+ localStorage.getItem("9")
+    ten.innerHTML =+ localStorage.getItem("10")
+    eleven.innerHTML =+ localStorage.getItem("11")
+    twelve.innerHTML =+ localStorage.getItem("12")
+    one.innerHTML =+ localStorage.getItem("1")
+    two.innerHTML =+ localStorage.getItem("2")
+    three.innerHTML =+ localStorage.getItem("3")
+    four.innerHTML =+ localStorage.getItem("4")
+    five.innerHTML =+ localStorage.getItem("5")
+
+    
 
     function updateTime() {
     const currentTime = moment().hour();
