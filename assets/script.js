1a8922ae93dae8e90239f29939164fb39ab914da
Noah Hildebrand <nhilde21@gmail.com>
2022-05-27 00:23:26 -0400

    fixed setting local storage

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,41 @@
 var today = moment();
 let timeBlock = document.querySelectorAll(".time-block")
-const nine = document.getElementById("9")
-const ten = document.getElementById("10")
-const eleven = document.getElementById("11")
-const twelve = document.getElementById("12")
-const one = document.getElementById("1")
-const two = document.getElementById("2")
-const three = document.getElementById("3")
-const four = document.getElementById("4")
-const five = document.getElementById("5")
+const nine = document.getElementById("#9am")
+const ten = document.getElementById("#10am")
+const eleven = document.getElementById("#11am")
+const twelve = document.getElementById("#12pm")
+const one = document.getElementById("#1pm")
+const two = document.getElementById("#2pm")
+const three = document.getElementById("#3pm")
+const four = document.getElementById("#4pm")
+const five = document.getElementById("#5pm")
 
 
 $(document).ready(function () {
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
     updateTime();
     $(".saveBtn").on("click", function () {
-        let plannerText = document.querySelectorAll(".description").value;
-        let time = timeBlock.getAttribute("id");
+        let plannerText = $(this).siblings(".description").val();
+        let time =  $(this).prev().attr("id")
         localStorage.setItem(time, plannerText)
 
     });
 
-    nine.innerHTML =+ localStorage.getItem("9")
-    ten.innerHTML =+ localStorage.getItem("10")
-    eleven.innerHTML =+ localStorage.getItem("11")
-    twelve.innerHTML =+ localStorage.getItem("12")
-    one.innerHTML =+ localStorage.getItem("1")
-    two.innerHTML =+ localStorage.getItem("2")
-    three.innerHTML =+ localStorage.getItem("3")
-    four.innerHTML =+ localStorage.getItem("4")
-    five.innerHTML =+ localStorage.getItem("5")
+    $(nine).text(localStorage.getItem("9"))
+    // ten.innerHTML =+ localStorage.getItem("10")
+    // eleven.innerHTML =+ localStorage.getItem("11")
+    // twelve.innerHTML =+ localStorage.getItem("12")
+    // one.innerHTML =+ localStorage.getItem("1")
+    // two.innerHTML =+ localStorage.getItem("2")
+    // three.innerHTML =+ localStorage.getItem("3")
+    // four.innerHTML =+ localStorage.getItem("4")
+    // five.innerHTML =+ localStorage.getItem("5")
 
     
 
     function updateTime() {
     const currentTime = moment().hour();
-    const blockTime = parseInt(timeBlock.getAttribute("id"))
+    const blockTime = parseInt($(this).attr("id"));
 
         timeBlock.forEach(function() {
             if(blockTime < currentTime) {
@@ -51,5 +51,5 @@ $(document).ready(function () {
         })
     }
 
-
+    
 });
